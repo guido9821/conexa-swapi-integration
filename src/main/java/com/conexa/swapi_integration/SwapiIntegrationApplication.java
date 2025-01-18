264aacd6c66e4f6630ba809e1646ed332a707912
Guido Barilatti <barilattiguidoa@gmail.com>
2025-01-18 17:33:13 -0300

    add PeopleService and its implementation
    Also add method for get all people and find people by id

--- src/main/java/com/conexa/swapi_integration/SwapiIntegrationApplication.java
+++ src/main/java/com/conexa/swapi_integration/SwapiIntegrationApplication.java
@@ -1,13 +1,21 @@
 package com.conexa.swapi_integration;
 
+import com.conexa.swapi_integration.service.PeopleService;
+import com.conexa.swapi_integration.service.impl.PeopleServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SwapiIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwapiIntegrationApplication.class, args);
+		PeopleService peopleService = new PeopleServiceImpl();
+            peopleService.findPeopleById(1);
+        SpringApplication.run(SwapiIntegrationApplication.class, args);
+
+
 	}
 
 }
--- src/main/java/com/conexa/swapi_integration/service/PeopleService.java
+++ src/main/java/com/conexa/swapi_integration/service/PeopleService.java
@@ -1,5 +1,9 @@
 package com.conexa.swapi_integration.service;
 
-public interface PeopleService {
+import com.conexa.swapi_integration.dto.PeopleDTO;
+import com.conexa.swapi_integration.model.ResponseWrapperPaged;
 
-}
+public interface PeopleService {
+        ResponseWrapperPaged<PeopleDTO> getAllPeople(int page, int limit);
+        PeopleDTO findPeopleById(int id);
+    }
