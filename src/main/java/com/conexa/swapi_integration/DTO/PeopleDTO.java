68f08a4e5829930d5ee90cedfa0aad9fa18706a4
Guido Barilatti <barilattiguidoa@gmail.com>
2025-01-18 01:09:59 -0300

    Add entities and DTOs

---
+++ src/main/java/com/conexa/swapi_integration/DTO/PeopleDTO.java
@@ -0,0 +1,105 @@
+package com.conexa.swapi_integration.DTO;
+
+import java.util.List;
+
+public class PeopleDTO {
+    private String name;
+    private String height;
+    private String mass;
+    private String hairColor;
+    private String skinColor;
+    private String eyeColor;
+    private String birthYear;
+    private String gender;
+    private List<String> films;
+    private List<String> starships;
+    private List<String> vehicles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getMass() {
+        return mass;
+    }
+
+    public void setMass(String mass) {
+        this.mass = mass;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<String> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<String> films) {
+        this.films = films;
+    }
+
+    public List<String> getStarships() {
+        return starships;
+    }
+
+    public void setStarships(List<String> starships) {
+        this.starships = starships;
+    }
+
+    public List<String> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<String> vehicles) {
+        this.vehicles = vehicles;
+    }
+}
---
+++ src/main/java/com/conexa/swapi_integration/DTO/SpeciesDTO.java
@@ -0,0 +1,115 @@
+package com.conexa.swapi_integration.DTO;
+
+import java.util.List;
+
+public class SpeciesDTO {
+
+    private String name;
+    private String classification;
+    private String designation;
+    private String averageHeight;
+    private String averageLifespan;
+    private String eyeColors;
+    private String hairColors;
+    private String skinColors;
+    private String language;
+    private String homeWorld;
+    private List<String> people;
+    private List<String> films;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getAverageHeight() {
+        return averageHeight;
+    }
+
+    public void setAverageHeight(String averageHeight) {
+        this.averageHeight = averageHeight;
+    }
+
+    public String getAverageLifespan() {
+        return averageLifespan;
+    }
+
+    public void setAverageLifespan(String averageLifespan) {
+        this.averageLifespan = averageLifespan;
+    }
+
+    public String getEyeColors() {
+        return eyeColors;
+    }
+
+    public void setEyeColors(String eyeColors) {
+        this.eyeColors = eyeColors;
+    }
+
+    public String getHairColors() {
+        return hairColors;
+    }
+
+    public void setHairColors(String hairColors) {
+        this.hairColors = hairColors;
+    }
+
+    public String getSkinColors() {
+        return skinColors;
+    }
+
+    public void setSkinColors(String skinColors) {
+        this.skinColors = skinColors;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getHomeWorld() {
+        return homeWorld;
+    }
+
+    public void setHomeWorld(String homeWorld) {
+        this.homeWorld = homeWorld;
+    }
+
+    public List<String> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<String> people) {
+        this.people = people;
+    }
+
+    public List<String> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<String> films) {
+        this.films = films;
+    }
+}
