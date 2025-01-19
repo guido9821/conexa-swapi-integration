9b5ba456eb8a4869d1c4d875e86b5ca79a9142f0
GabO9821 <barilattiguidoa@gmail.com>
2025-01-19 17:24:26 -0300

    Add search methods
    Add search method by name and model for Starships

--- src/main/java/com/conexa/swapi_integration/controller/StarshipController.java
+++ src/main/java/com/conexa/swapi_integration/controller/StarshipController.java
@@ -6,6 +6,8 @@ import com.conexa.swapi_integration.service.StarshipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class StarshipController {
@@ -21,5 +23,15 @@ public class StarshipController {
     @GetMapping("/starships/{id}")
     public StarshipDTO getPersonById(@PathVariable int id) {
         return starshipService.findStarshipById(id);
+    }
+
+    @GetMapping("/starships/searchByName/")
+    public List<StarshipDTO> searchByName(@RequestParam String name) {
+        return starshipService.findStarshipsByName(name);
+    }
+
+    @GetMapping("/starships/searchByModel/")
+    public List<StarshipDTO> searchByModel(@RequestParam String model) {
+        return starshipService.findStarshipsByModel(model);
     }
 }
