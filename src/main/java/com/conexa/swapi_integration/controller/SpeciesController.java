dddd8823ec57435b0d583df6942a4fbe1ab83d30
GabO9821 <barilattiguidoa@gmail.com>
2025-01-19 13:01:12 -0300

    Create SpeciesController

---
+++ src/main/java/com/conexa/swapi_integration/controller/SpeciesController.java
@@ -0,0 +1,24 @@
+package com.conexa.swapi_integration.controller;
+
+import com.conexa.swapi_integration.dto.SpeciesDTO;
+import com.conexa.swapi_integration.model.ResponseWrapperPaged;
+import com.conexa.swapi_integration.service.SpeciesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class SpeciesController {
+    @Autowired
+    SpeciesService speciesService;
+
+    @GetMapping("/species/")
+    public ResponseWrapperPaged<SpeciesDTO> getAllSpecies(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5")int limit){
+        return speciesService.getAllSpecies(page, limit);
+    }
+
+    @GetMapping("/species/{id}")
+    public SpeciesDTO getPersonById(@PathVariable int id) {
+        return speciesService.findSpeciesById(id);
+    }
+}
