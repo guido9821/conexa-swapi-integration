80d4dfebd85b3a9649bb4768a7c366aa47db8f79
GabO9821 <barilattiguidoa@gmail.com>
2025-01-19 17:41:05 -0300

    Add search method for Species
    Add search method to search Species by name

--- src/main/java/com/conexa/swapi_integration/service/impl/SpeciesServiceImpl.java
+++ src/main/java/com/conexa/swapi_integration/service/impl/SpeciesServiceImpl.java
@@ -1,12 +1,10 @@
 package com.conexa.swapi_integration.service.impl;
 
-import com.conexa.swapi_integration.dto.FilmDTO;
-import com.conexa.swapi_integration.dto.PeopleDTO;
-import com.conexa.swapi_integration.dto.SpeciesDTO;
 import com.conexa.swapi_integration.dto.SpeciesDTO;
 import com.conexa.swapi_integration.model.ResponseWrapper;
 import com.conexa.swapi_integration.model.ResponseWrapperPaged;
 import com.conexa.swapi_integration.service.SpeciesService;
+import com.conexa.swapi_integration.util.MapperUtil;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class SpeciesServiceImpl implements SpeciesService {
@@ -46,6 +45,17 @@ public class SpeciesServiceImpl implements SpeciesService {
             throw new RuntimeException(e);
         }
         return null;
+    }
+
+    @Override
+    public List<SpeciesDTO> findSpeciesByName(String name) {
+        ResponseEntity<String> responseEntityRaw = restTemplate.exchange(
+                BASE_URL_SPECIES + "?name=" + name, HttpMethod.GET, null, String.class);
+        try {
+            return MapperUtil.getObjectListFromJson(responseEntityRaw, SpeciesDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
