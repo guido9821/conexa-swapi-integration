a4636fa7c42a35358c1453b16bb77f9614a95379
Guido Barilatti <barilattiguidoa@gmail.com>
2025-01-20 17:44:45 -0300

    Add test for class PeopleServiceImp

---
+++ src/test/java/com/conexa/swapi_integration/serviceTest/PeopleServiceTest.java
@@ -0,0 +1,130 @@
+package com.conexa.swapi_integration.serviceTest;
+
+import com.conexa.swapi_integration.dto.PeopleDTO;
+import com.conexa.swapi_integration.model.ResponseWrapperPaged;
+import com.conexa.swapi_integration.service.impl.PeopleServiceImpl;
+import com.conexa.swapi_integration.util.MapperUtil;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PeopleServiceTest {
+
+    @InjectMocks
+    private PeopleServiceImpl peopleServiceImpl;
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getAllPeopleOkTest() {
+
+        ResponseWrapperPaged<PeopleDTO> mockResponse = new ResponseWrapperPaged<>();
+        mockResponse.setResults(Collections.singletonList(new PeopleDTO()));
+
+        ResponseEntity<ResponseWrapperPaged<PeopleDTO>> mockEntity = mock(ResponseEntity.class);
+        when(mockEntity.getBody()).thenReturn(mockResponse);
+
+        when(restTemplate.exchange(
+                eq("https://www.swapi.tech/api/people/?page=1&limit=10"),
+                eq(HttpMethod.GET),
+                isNull(),
+                any(ParameterizedTypeReference.class))
+        ).thenReturn(mockEntity);
+
+        ResponseWrapperPaged<PeopleDTO> result = peopleServiceImpl.getAllPeople(1, 10);
+
+        assertNotNull(result);
+        assertNotNull(result.getResults());
+        assertEquals(1, result.getResults().size());
+        verify(restTemplate, times(1)).exchange(
+                eq("https://www.swapi.tech/api/people/?page=1&limit=10"),
+                eq(HttpMethod.GET),
+                isNull(),
+                any(ParameterizedTypeReference.class)
+        );
+    }
+
+    @Test
+    public void getAllPeopleNullTest() {
+
+        ResponseEntity<ResponseWrapperPaged<PeopleDTO>> mockEntity = new ResponseEntity<>(null, HttpStatus.OK);
+
+        when(restTemplate.exchange(
+                eq("https://www.swapi.tech/api/people/?page=1&limit=10"),
+                eq(HttpMethod.GET),
+                isNull(),
+                any(ParameterizedTypeReference.class))
+        ).thenReturn(mockEntity);
+
+        ResponseWrapperPaged<PeopleDTO> result = peopleServiceImpl.getAllPeople(1, 10);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void findPeopleByIdOkTest() {
+
+        String jsonResponse = "{\"result\":{\"properties\":{\"name\":\"Luke Skywalker\"}}}";
+        ResponseEntity<String> responseEntityRaw = new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(), any(Class.class))).thenReturn(responseEntityRaw);
+
+        PeopleDTO actualPerson = peopleServiceImpl.findPeopleById(1);
+
+        PeopleDTO expectedPerson = new PeopleDTO();
+        expectedPerson.setName("Luke Skywalker");
+
+        assertEquals(expectedPerson.getName(), actualPerson.getName());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void findPeopleByIdExceptionTest() throws IOException {
+
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(), any(Class.class))).thenReturn(new ResponseEntity<String>("", HttpStatus.OK));
+        when(MapperUtil.getObjectFromJson(any(), any())).thenThrow(new IOException("Error de prueba"));
+
+        peopleServiceImpl.findPeopleById(1);
+    }
+
+    @Test
+    public void findPeopleNameIdOkTest() {
+
+        String jsonResponse = "{\"result\":[{\"properties\":{\"name\":\"Luke Skywalker\"}}]}";
+        ResponseEntity<String> responseEntityRaw = new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(), any(Class.class))).thenReturn(responseEntityRaw);
+
+        List<PeopleDTO> actualPerson = peopleServiceImpl.findPeopleByName("Luke Skywalker");
+
+        PeopleDTO expectedPerson = new PeopleDTO();
+        expectedPerson.setName("Luke Skywalker");
+
+        assertEquals(expectedPerson.getName(), actualPerson.get(0).getName());
+    }
+}
+
+
