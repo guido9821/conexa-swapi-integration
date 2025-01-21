61ef94caa0e8ce74a602d2ddbe99e72b7efca6d5
Guido Barilatti <barilattiguidoa@gmail.com>
2025-01-21 16:22:42 -0300

    Add swagger documentation for DTOs

--- src/main/java/com/conexa/swapi_integration/dto/PeopleDTO.java
+++ src/main/java/com/conexa/swapi_integration/dto/PeopleDTO.java
@@ -2,33 +2,53 @@ package com.conexa.swapi_integration.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "Datos de una persona del universo de Star Wars")
 public class PeopleDTO {
 
+    @Schema(description = "Nombre de la persona", example = "Luke Skywalker")
     private String name;
+
+    @Schema(description = "Altura de la persona en centímetros", example = "172")
     private String height;
+
+    @Schema(description = "Masa de la persona en kilogramos", example = "77")
     private String mass;
 
+    @Schema(description = "Color de pelo de la persona", example = "blond")
     @JsonProperty("hair_color")
     private String hairColor;
 
+    @Schema(description = "Color de piel de la persona", example = "fair")
     @JsonProperty("skin_color")
     private String skinColor;
 
+    @Schema(description = "Color de ojos de la persona", example = "blue")
     @JsonProperty("eye_color")
     private String eyeColor;
 
+    @Schema(description = "Año de nacimiento de la persona", example = "19BBY")
     @JsonProperty("birth_year")
     private String birthYear;
 
+    @Schema(description = "Género de la persona", example = "male")
     private String gender;
+
+    @Schema(description = "Fecha de creación del registro", example = "2014-12-09T13:50:51.644000Z")
     private String created;
+
+    @Schema(description = "Fecha de última edición del registro", example = "2014-12-20T21:17:56.891000Z")
     private String edited;
+
+    @Schema(description = "URL del planeta natal de la persona", example = "https://swapi.dev/api/planets/1/")
     @JsonProperty("homeworld")
     private String homeWorld;
+
+    @Schema(description = "URL del recurso de la persona", example = "https://swapi.dev/api/people/1/")
     private String url;
 
     public String getCreated() {
--- src/main/java/com/conexa/swapi_integration/dto/ResultDTO.java
+++ src/main/java/com/conexa/swapi_integration/dto/ResultDTO.java
@@ -1,10 +1,16 @@
 package com.conexa.swapi_integration.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ResultDTO <T>{
+@Schema(description = "Objeto que encapsula propiedades y una descripción (ej. para resultados de búsqueda)")
+public class ResultDTO<T> {
+
+    @Schema(description = "Propiedades del resultado", example = "{\"name\": \"Luke Skywalker\", \"height\": \"172\", ...}") // Example as JSON string
     private T properties;
+
+    @Schema(description = "Descripción del resultado", example = "Información sobre Luke Skywalker")
     private String description;
 
     public String getDescription() {
